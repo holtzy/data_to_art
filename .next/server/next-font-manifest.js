423e004c99634e6b27ec152e97eb0c16bd91f773
holtzy <yan.holtz@datadoghq.com>
2025-08-07 16:25:24 +0000

    Deploying to gh-pages from @ holtzy/data_to_art@c4a092f6ee75e49d305347f8edfa8321d14d1a7e 🚀

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/yanholtz/Desktop/data_to_art/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
