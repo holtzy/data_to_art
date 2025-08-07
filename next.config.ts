2ad43cf5723767a6c1849c3e415f2107e1dbbafb
Holtz Yan <yan.holtz@datadoghq.com>
2025-08-07 17:09:11 +0200

    k

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true }, // GitHub Pages can't optimize images
-  basePath: '/data_to_art',   // 👈 Important: see next step
-  assetPrefix: '/data_to_art',
+  basePath: '/data_to_art/',
+  assetPrefix: '/data_to_art/',
 
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
