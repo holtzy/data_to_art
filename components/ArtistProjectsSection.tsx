a5d14aa2f019edff1d614b770772fc031e618ede
Holtz Yan <yan.holtz@datadoghq.com>
2025-08-20 15:21:52 +0200

    k

--- components/ArtistProjectsSection.tsx
+++ components/ArtistProjectsSection.tsx
@@ -1,5 +1,6 @@
-import { projectList } from "@/lib/project-list";
+import { Project, projectList } from "@/lib/project-list";
 import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 
 type ArtistProjectsSectionProps = { artistId: string };
 
@@ -9,29 +10,45 @@ export const ArtistProjectsSection = ({
   return (
     <>
       <h2 className="mb-6">Projects</h2>
-      <div className="flex flex-wrap gap-6 mb-12">
+      <div className="grid grid-cols-2 gap-4">
         {projectList
           .filter((p) => p.artist === artistId)
           .map((p, i) => (
-            <Link
-              key={i}
-              href={`/artist/${artistId}/${p.folder}`}
-              className="w-full sm:w-1/2 lg:w-1/4 cursor-pointer group"
-            >
-              <div className="flex flex-col gap-2">
-                <img
-                  src={`/project/${artistId}/${p.folder}/01-thumb.webp`}
-                  alt={`${p.name} project thumbnail`}
-                  className="rounded-md transition-transform group-hover:scale-105"
-                />
-                <span className="font-semibold text-lg">{p.name}</span>
-                <div className="text-xs text-slate-500">
-                  {p.descriptionShort}
-                </div>
-              </div>
-            </Link>
+            <div key={i} className="">
+              <ProjectCard project={p} />
+            </div>
           ))}
       </div>
     </>
+  );
+};
+
+type ProjectCardProps = {
+  project: Project;
+};
+
+export const ProjectCard = ({ project }: ProjectCardProps) => {
+  const { folder, name, descriptionShort, artist } = project;
+
+  return (
+    <div className="flex flex-col gap-2">
+      <span className="font-semibold text-lg no-underline">{name}</span>
+      <Link href={`/artist/${artist}/${folder}`} className="w-full">
+        <img
+          src={`/project/${artist}/${folder}/01-thumb.webp`}
+          alt={`${name} project thumbnail`}
+          className="h-58 object-cover"
+        />
+      </Link>
+      <div className="text-xs text-slate-500">{descriptionShort}</div>
+      <div>
+        <Link
+          className={buttonVariants({ variant: "outline", size: "sm" })}
+          href={`/artist/${artist}/${folder}`}
+        >
+          Read
+        </Link>
+      </div>
+    </div>
   );
 };
