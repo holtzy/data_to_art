2a684cfc11719bd05c937a0af9215c5c92bf1c60
Holtz Yan <yan.holtz@datadoghq.com>
2025-08-20 12:28:39 +0200

    k

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,27 +5,33 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <>
-      <div className="flex flex-wrap gap-2">
-        {projectList.map((p, i) => {
-          return (
-            <Link
-              key={i}
-              href={"/project/" + p.artist + "/" + p.folder}
-              className="rounded-md hover:bg-slate-50 flex flex-col justify-center items-start gap-4"
-            >
-              <img
-                src={
-                  "/project/" + p.artist + "/" + p.folder + "/01-medium.webp"
-                }
-                className="w-40 h-40"
-              />
-              <span>{p.name}</span>
-              <span>{p.artist}</span>
-            </Link>
-          );
-        })}
+    <div>
+      <div className=" my-44 flex flex-col items-center bg-gradient-to-t from-transparent to-white">
+        <h1 className="text-9xl">Projects</h1>
+        <p className="text-center max-w-62">{`kkk`}</p>
       </div>
-    </>
+      <div className="wrapper">
+        <div className="flex flex-wrap gap-2">
+          {projectList.map((p, i) => {
+            return (
+              <Link
+                key={i}
+                href={"/project/" + p.artist + "/" + p.folder}
+                className="rounded-md hover:bg-slate-50 flex flex-col justify-center items-start gap-4"
+              >
+                <img
+                  src={
+                    "/project/" + p.artist + "/" + p.folder + "/01-medium.webp"
+                  }
+                  className="w-44 h-64 object-cover"
+                />
+                <span>{p.name}</span>
+                <span>{p.artist}</span>
+              </Link>
+            );
+          })}
+        </div>
+      </div>{" "}
+    </div>
   );
 }
