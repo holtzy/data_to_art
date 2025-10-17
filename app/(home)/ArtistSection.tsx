307c1b34c89eff996b9dc69f1ddb8db504429f0e
Holtz Yan <yan.holtz@datadoghq.com>
2025-10-17 10:07:57 +0200

    add contact section below artworks

--- app/(home)/ArtistSection.tsx
+++ app/(home)/ArtistSection.tsx
@@ -8,6 +8,7 @@ import { Activity, Home, Linkedin } from "lucide-react";
 import { BubbleMap } from "@/components/viz/bubbleMap/BubbleMap";
 import AvatarBeeswarm from "@/components/AvatarBeeswarm";
 import { buttonVariants } from "@/components/ui/button";
+import { EmailButton } from "@/components/EmailButton";
 
 export const ArtistSection = () => {
   const [hovered, setHovered] = useState<null | Artist>(null);
@@ -78,17 +79,19 @@ export const ArtistSection = () => {
       </div>
 
       <div className="w-full flex gap-2 justify-center my-4">
+        <EmailButton
+          name={"Suggest an Artist"}
+          variant="outline"
+          link="wakeupdataviz@gmail.com"
+        />
         <Link
           href={"/artists"}
           className={cn(
-            buttonVariants({ size: "lg", variant: "outline" }),
+            buttonVariants({ size: "lg", variant: "default" }),
             "mb-12"
           )}
         >
           See all artists
-        </Link>
-        <Link href={"/artists"} className={cn(buttonVariants({ size: "lg" }))}>
-          Suggest an artist
         </Link>
       </div>
     </div>
--- app/artworks/page.tsx
+++ app/artworks/page.tsx
@@ -5,6 +5,7 @@ import { shuffleArray } from "@/lib/utils";
 import { projectList } from "@/lib/project-list";
 import { artistList } from "@/lib/artist-list";
 import GalleryClient from "./GalleryClient";
+import { Contact } from "@/components/Contact";
 
 export default function ArtworksPage() {
   const publicDir = path.join(process.cwd(), "public", "project");
@@ -35,18 +36,21 @@ export default function ArtworksPage() {
   ];
 
   return (
-    <div className="relative mt-52 max-w-[900px] mx-auto">
-      <div className="mt-8">
-        <GalleryClient imgPaths={imgPaths} />
-      </div>
+    <>
+      <div className="relative mt-52 max-w-[900px] mx-auto">
+        <div className="mt-8">
+          <GalleryClient imgPaths={imgPaths} />
+        </div>
 
-      <div className="absolute top-0 w-full h-[400px] flex flex-col items-center bg-gradient-to-t from-transparent to-white">
-        <h1 className="text-9xl">The Wall</h1>
-        <p className="text-center max-w-62">
-          {`Data to Art features ${projectList.length} projects from ${artistList.length} artists. That’s ${items.length} unique images waiting to be explored below. Any other suggestion? `}
-          <Link href="/about">Contact us</Link>
-        </p>
+        <div className="absolute top-0 w-full h-[400px] flex flex-col items-center bg-gradient-to-t from-transparent to-white">
+          <h1 className="text-9xl">The Wall</h1>
+          <p className="text-center max-w-62">
+            {`Data to Art features ${projectList.length} projects from ${artistList.length} artists. That’s ${items.length} unique images waiting to be explored below. Any other suggestion? `}
+            <Link href="/about">Contact us</Link>
+          </p>
+        </div>
       </div>
-    </div>
+      <Contact />
+    </>
   );
 }
