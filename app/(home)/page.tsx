a22439da2c87b4205e00a8bf6615158c0bbf37ae
Holtz Yan <yan.holtz@datadoghq.com>
2025-12-18 16:33:39 +0100

    exhibition page ok

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -257,7 +257,7 @@ export default function Home() {
             {/* top centered button */}
             <div className="absolute top-1/2 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
               <Link
-                href="/exhibitions"
+                href="/exhibition"
                 className={buttonVariants({ size: "lg" })}
               >
                 See all exhibitions
--- app/exhibition/page.tsx
+++ app/exhibition/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { EmailButton } from "@/components/EmailButton";
 import { DataArtEventTable } from "./DataArtEventTable";
 import { Contact } from "@/components/Contact";
 import { Spacing } from "@/components/Spacing";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function ExibPage() {
   return (
@@ -17,7 +19,12 @@ export default function ExibPage() {
           exploration.
         </p>
         <div className="mt-10 flex gap-2">
-          <Button variant={"outline"}>See events</Button>
+          <Link
+            href="#table"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            See events
+          </Link>
           <Button>Add an event</Button>
         </div>
       </div>
@@ -26,20 +33,59 @@ export default function ExibPage() {
 
       <div className="wrapper">
         <p>
+          {" "}
           This page brings together a curated selection of past, ongoing, and
           upcoming exhibitions where data visualization plays a central role —
           whether as the core of the exhibition or as a key component within a
-          broader curatorial project. From fully data-driven installations to
-          exhibitions that integrate maps, infographics, and visual narratives,
-          this listing reflects the growing presence of data as a cultural and
-          expressive medium.
+          broader curatorial project.
+        </p>
+      </div>
+
+      <div className="full-bleed my-12 grid gap-2">
+        <div className="grid grid-cols-2 gap-2">
+          <img
+            src="/asset/mockup1.webp"
+            className="h-[400px] w-full object-cover"
+            alt="Mockup 1"
+          />
+          <img
+            src="/asset/mockup2.webp"
+            className="h-[400px] w-full object-cover"
+            alt="Mockup 2"
+          />
+        </div>
+      </div>
+
+      <div className="wrapper">
+        <p>
+          From fully data-driven installations to exhibitions that integrate
+          maps, infographics, and visual narratives, this listing reflects the
+          growing presence of data as a cultural and expressive medium.
         </p>
       </div>
 
       <Spacing />
 
-      <div className="max-w-[1100px] mx-auto">
+      <div id="table" className="max-w-[1100px] mx-auto scroll-mt-48">
         <DataArtEventTable />
+      </div>
+
+      <Spacing />
+
+      <div className="wrapper">
+        <p>
+          If you know of an event that is not listed here, feel free to reach
+          out so we can help make this list as complete as possible. And if you
+          would like support organizing an exhibition, send us a message too. We
+          would be happy to help.
+        </p>
+        <div className="mt-4">
+          <EmailButton
+            name="Contact us"
+            variant="default"
+            link="wakeupdataviz@gmail.com"
+          />
+        </div>
       </div>
 
       <Spacing />
