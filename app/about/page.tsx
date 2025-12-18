90f9d727bd00d38b8c609d4230b4e15d29cecdd8
Holtz Yan <yan.holtz@datadoghq.com>
2025-12-18 19:05:13 +0100

    k

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,77 +1,165 @@
 import { QuoteSection } from "@/components/QuoteSection";
-import SocialMediaButtons from "@/components/SocialMediaButtons";
+import SocialMediaButtons, {
+  SocialMediaButtonsMarthe,
+} from "@/components/SocialMediaButtons";
+import { Spacing } from "@/components/Spacing";
+import { ArtScienceCursorSection } from "../(home)/ArtScienceCursorSection";
+import Link from "next/link";
+import { EmailButton } from "@/components/EmailButton";
 
 export default function AboutPage() {
   return (
     <>
       <div className="wrapper mt-40">
-        <h1>About Data To Art</h1>
-        <p>
-          Data to Art began as a meeting — between a curator and a dataviz
-          passionate fascinated by the emotional side of data.
-        </p>
-        <p>
-          At the crossroads of exhibition design, digital creation, and visual
-          exploration, we imagine spaces where information turns into matter,
-          color, and emotion.
-        </p>
+        <center>
+          <h1>About</h1>
+          <p className="max-w-96">
+            Data to Art began as a meeting between a curator and a dataviz
+            passionate. Both fascinated by the emotional side of data.
+          </p>
+        </center>
       </div>
 
-      <section className="max-w-[800px] mx-auto mt-16">
-        <div className="flex justify-center items-start gap-8">
-          {/* Yan */}
-          <div className="flex flex-col justify-start items-end text-right w-1/2">
-            <img
-              src="/asset/yan-holtz.png"
-              alt="A picture of Yan Holtz"
-              width={120}
-              height={120}
-              className="rounded-full mb-2"
-            />
-            <div className="mb-4 mt-2">{YanHoltzDescription}</div>
-            <div className="opacity-60 hover:opacity-100">
-              <SocialMediaButtons />
+      <Spacing />
+
+      <img src="/expo/expo2.jpg" className="px-10" />
+      <Spacing />
+
+      <section className="wrapper">
+        <h1>What is Data Art</h1>
+        <p className="drop-cap">
+          Data art is a creative practice that transforms raw data into visually
+          compelling artworks. By blending aesthetics with information, data
+          artists use charts, patterns, and interactive visuals to reveal
+          stories, emotions, and insights hidden within datasets.{" "}
+        </p>
+        <p className="mb-12">
+          Data art acts as a cursor between science and abstraction. Some works
+          feel like scientific figures, with annotations and numbers that speak
+          for themselves. Others drift into pure abstraction, only revealing
+          their meaning with extra context.
+        </p>
+        <ArtScienceCursorSection />
+        <p>
+          While data art and generative art both involve digital creativity,
+          they are not the same. Data art specifically uses real-world data as
+          its source material, aiming to visualize information, patterns, or
+          stories embedded in that data.
+        </p>
+        <QuoteSection
+          text="Data art reveals meaning from datasets, while generative art
+                emphasizes process, randomness, and system-driven creativity."
+        />
+      </section>
+
+      <Spacing />
+
+      <section className="wrapper bg-slate-50 py-24">
+        <h1>Why this website?</h1>
+
+        <p>
+          We've long been fascinated by the many ways information can be
+          represented. Some artists use data as a medium to create truly
+          beautiful works.
+        </p>
+
+        <p>
+          But these masterpieces are scattered across the internet, with no
+          central place to discover or showcase them. That's why we created{" "}
+          <Link href="/">Data to Art</Link>.
+        </p>
+
+        <p>
+          Marthe has curated Data Art for years and knows its community and
+          languages inside out. Yan is a software engineer passionate about
+          dataviz. Bringing our skills together to build this project was a
+          natural step.
+        </p>
+
+        <p>
+          We carefully select the best data artists and present their work in a
+          thoughtful, beautiful way.
+        </p>
+
+        <p>
+          Data to Art is free, hybrid, and inclusive. It celebrates diverse
+          approaches and styles, offering a space for creators to experiment and
+          innovate. We invite you to experience data differently — to see its
+          beauty, and to discover how it can move, inspire, and spark
+          reflection.
+        </p>
+
+        <Spacing />
+
+        <section>
+          <div className="max-w-[800px] mx-auto mt-16">
+            <div className="flex justify-center items-start gap-8">
+              {/* Yan */}
+              <div className="flex flex-col justify-start items-end text-right w-1/2">
+                <img
+                  src="/asset/yan-holtz.png"
+                  alt="A picture of Yan Holtz"
+                  width={120}
+                  height={120}
+                  className="rounded-full mb-2"
+                />
+                <div className="mb-4 mt-2">{YanHoltzDescription}</div>
+                <div className="opacity-60 hover:opacity-100">
+                  <SocialMediaButtons />
+                </div>
+              </div>
+
+              {/* Marthe */}
+              <div className="flex flex-col justify-start items-start w-1/2">
+                <img
+                  src="/asset/marthe-viallet.jpg"
+                  alt="A picture of Marthe Viallet"
+                  width={120}
+                  height={120}
+                  className="rounded-full mb-2"
+                />
+                <div className="mb-4 mt-2">{MartheDescription}</div>
+                <div className="opacity-60 hover:opacity-100">
+                  <SocialMediaButtonsMarthe />
+                </div>
+              </div>
             </div>
           </div>
+        </section>
+      </section>
 
-          {/* Marthe */}
-          <div className="flex flex-col justify-start items-start w-1/2">
-            <img
-              src="/asset/marthe-viallet.jpg"
-              alt="A picture of Marthe Viallet"
-              width={120}
-              height={120}
-              className="rounded-full mb-2"
+      <Spacing />
+
+      <section>
+        <div className="wrapper mt-22">
+          <h1>Create an exhibition</h1>
+          <p>
+            A data-art exhibition is more than a visual event: it’s a form of
+            mediation, a collective experience, and a meeting point between art,
+            science, and society.
+          </p>
+          <p>
+            By giving data a physical and poetic form, we make visible what
+            usually remains abstract — climate, health, mobility, human emotion.
+            These works awaken curiosity, wonder, and reflection, redefining our
+            relationship to information.
+          </p>
+          <p>
+            For institutions, cultural festivals, and public organizations, data
+            exhibitions are a powerful tool for awareness, innovation, and
+            dialogue. They turn data into a sensitive language and open new
+            conversations between experts and citizens. Because when data is
+            well told, it can change the way we see the world.
+          </p>
+          <div className="my-8">
+            <EmailButton
+              name="Contact"
+              variant="default"
+              link="wakeupdataviz@gmail.com"
             />
-            <div className="mb-4 mt-2">{MartheDescription}</div>
-            <div className="opacity-60 hover:opacity-100">
-              <SocialMediaButtons />
-            </div>
           </div>
         </div>
       </section>
-
-      <div className="wrapper mt-22">
-        <h3>Let's create an exhibition</h3>
-        <p>
-          A data-art exhibition is more than a visual event: it’s a form of
-          mediation, a collective experience, and a meeting point between art,
-          science, and society.
-        </p>
-        <p>
-          By giving data a physical and poetic form, we make visible what
-          usually remains abstract — climate, health, mobility, human emotion.
-          These works awaken curiosity, wonder, and reflection, redefining our
-          relationship to information.
-        </p>
-        <p>
-          For institutions, cultural festivals, and public organizations, data
-          exhibitions are a powerful tool for awareness, innovation, and
-          dialogue. They turn data into a sensitive language and open new
-          conversations between experts and citizens. Because when data is well
-          told, it can change the way we see the world.
-        </p>
-      </div>
     </>
   );
 }
